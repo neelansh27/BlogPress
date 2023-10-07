f8eb1d12bc4d7877d37c01876d285ece7f469920
neelansh27 <neelanshsharma30@gmail.com>
2023-10-07 19:58:05 +0530

    Initialised react app

---
+++ BlogPress/client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/navbar'  
+function App() {
+  return (
+    <div >
+      <header >
+        <Navbar/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
