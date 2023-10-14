1b0769e5d1698d1a7d9fa140654a918469a32404
neelansh27 <neelanshsharma30@gmail.com>
2023-10-14 16:55:46 +0530

    Added create and read operations for posts

--- BlogPress/server/routes/routes.js
+++ BlogPress/server/routes/routes.js
@@ -1,22 +1,84 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const {User,Post} = require('../schema/models')
+const {User, Posts} = require("../schema/models");
 
 mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database Connected");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.DB_URI, {
+      useNewUrlParser : true,
+      useUnifiedTopology : true,
+    })
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(err));
 
-router.get("/", (req, res) => {
-  res.json({ everything: "good" });
+router.post("/register", (req, res) => {
+  const newUser = new User({
+    username : req.body.username,
+    email : req.body.email,
+    password : req.body.password,
+  });
+  newUser.save()
+      .then(() => {
+        console.log("Added " + newUser._id);
+        res.json({Added : newUser._id});
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
 });
 
+// Using user Id to add posts
+router.post("/post/add", (req, res) => {
+  User.findById(req.body.uid)
+      .then((entry) => {
+        const newPost = new Posts({
+          author_id : entry._id,
+          author : entry.username,
+          title : req.body.title,
+          content : req.body.content,
+        });
+        newPost.save()
+            .then(() => {
+              res.json({
+                post_id : newPost._id,
+                Status : "Added Successfully",
+              });
+            })
+            .catch((err) => { res.json(err); });
+      })
+      .catch((err) => res.json(err));
+});
+
+router.get("/post/getposts", (req, res) => {
+  Posts.find({}, "-author_id -__v")
+      .sort({createdAt : -1})
+      .skip(5 * (req.query.page - 1 || 0))
+      .limit(5)
+      .then((posts) => { res.json(posts); })
+      .catch((err) => { res.json(err); });
+});
+
+router.get("/post/get/:id", (req, res) => {
+  Posts.findById(req.params.id, "-author_id -__v")
+      .then((post) => res.json(post))
+      .catch((err) => res.json(err));
+});
+
+// Using post Id to add comments
+router.post("/comment/add", (req, res) => {
+  Posts.findById(req.body.pid)
+      .then((entry) => {
+        const comment = {
+          author : req.body.author,
+          content : req.body.content,
+        };
+        entry.comments.push(comment);
+        entry.save()
+            .then(() => { res.json({status : "Added Comment Successfully"}); })
+            .catch((err) => res.json(err));
+      })
+      .catch((err) => res.json(err));
+});
 module.exports = router;
--- BlogPress/server/schema/models.js
+++ BlogPress/server/schema/models.js
@@ -4,22 +4,34 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  profilePic:Buffer,
+  profilePic: Buffer,
+});
+
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: String,
+    required: true,
+  },
+  content: String,
 });
 
 const postSchema = new mongoose.Schema({
-  author: {
+  author_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
+    required: true,
   },
+  author:String,
   title: { type: String, required: true },
   content: String,
+  comments:[commentSchema],
   createdAt: {
     type: Date,
     default: Date.now,
   },
 });
 
+
 const User = mongoose.model("User", userSchema);
-const Posts = mongoose.model("Post",postSchema);
-module.exports = {User,Posts}
+const Posts = mongoose.model("Post", postSchema);
+module.exports = { User, Posts };
