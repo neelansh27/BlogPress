8813049cbfd3420bda2620b5e9353fa768c72faf
neelansh27 <neelanshsharma30@gmail.com>
2023-10-22 20:34:24 +0530

    Add navbar css

--- BlogPress/client/src/components/navbar.js
+++ BlogPress/client/src/components/navbar.js
@@ -1,16 +1,19 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import {Link} from "react-router-dom"
 import {AuthContext} from "../context/auth.context"
+import "../css/navbar.css"
 function Navbar() {
   const {logout} = useContext(AuthContext);
   return (
-    <header>
+    <header className="nav-container">
       <nav>
         <ul>
-          <li><Link to="/">BlogPress</Link></li>
-          <li><Link to="/post/create">Create Post</Link></li>
-          <li><Link to="/profile">Profile</Link></li>
-          <li><button onClick={()=>logout()}>Logout</button></li>
+          <li><Link className="anchor" to="/">BlogPress</Link></li>
+          <li><Link className="anchor" to="/post/create">Create Post</Link></li>
+          <li><Link className="anchor" to="/profile">Profile</Link></li>
+          <li><button onClick={()=>logout()}>
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+    </button></li>
         </ul>
       </nav>
     </header>
