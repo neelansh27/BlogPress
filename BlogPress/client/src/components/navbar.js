7530c082918db8080f6893ad1fe5309ce64685f0
neelansh27 <neelanshsharma30@gmail.com>
2023-10-22 14:25:32 +0530

    Add Logout option to navbar

--- BlogPress/client/src/components/navbar.js
+++ BlogPress/client/src/components/navbar.js
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import {Link} from "react-router-dom"
+import {AuthContext} from "../context/auth.context"
 function Navbar() {
+  const {logout} = useContext(AuthContext);
   return (
     <header>
       <nav>
         <ul>
           <li><Link to="/">BlogPress</Link></li>
+          <li><Link to="/post/create">Create Post</Link></li>
+          <li><button onClick={()=>logout()}>Logout</button></li>
         </ul>
       </nav>
     </header>
