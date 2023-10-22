2be96974772af8bf93255bc486ccadf5a5563271
neelansh27 <neelanshsharma30@gmail.com>
2023-10-22 22:02:57 +0530

    Fix bugs and Add home page css

--- BlogPress/client/src/components/post.js
+++ BlogPress/client/src/components/post.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import "../css/post.css"
 
 export default function Post() {
   const [post, setPost] = useState(null);
@@ -16,8 +17,8 @@ export default function Post() {
       `http://localhost:3000/post/getposts?page=${page}&search=${query}`
     )
       .then((res) => res.json())
-      .then((posts) => {
-        setPost(posts);
+      .then((data) => {
+        setPost(data.posts);
       });
   }
   useEffect(() => {
@@ -38,31 +39,39 @@ export default function Post() {
   }
   return (
     <main>
+    <div class="searchBar">
       <input
         type="text"
         value={query}
         name="search"
+        placeholder="Search Posts..."
         onChange={handleInput}
         autoComplete="off"
       />
-      <button onClick={search}>O</button>
+      <button onClick={search}>
+        <svg class="svg-icon search-icon" aria-labelledby="title desc" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 19.9 19.7"><title id="title">Search Icon</title><desc id="desc">A magnifying glass icon.</desc><g className="search-path" fill="none" stroke="#343434"><path strokeLinecap="square" d="M18.5 18.3l-5.4-5.4"/><circle cx="8" cy="8" r="7"/></g></svg>
+    </button>
+</div>
+    <div className="post-container">
       {!post ||
         post.map((item) => {
           return (
-            <div key={item._id}>
-              <div>{item.title}</div>
-              <div>{item.author}</div>
-              <div>Posted on {(new Date(item.createdAt)).toLocaleDateString()}</div>
+            <div className="post" key={item._id}>
+              <div className="post-title">{item.title}</div>
+              <small className="post-author">By {item.author}</small>
+              <small className="post-creation">Posted on {(new Date(item.createdAt)).toLocaleDateString()}</small>
             <div>
-              <Link to={`/post/${item._id}`}>View post</Link>
+              <Link className="post-view" to={`/post/${item._id}`}>View post</Link>
             </div>
-              <hr />
             </div>
           );
         })
       }
+  </div>
+    <div className="nav-buttons">
     {page!==1 && <button onClick={handlePagePrev}>Prev</button>}
-    {page!==total && <button onClick={handlePageNext}>Next Page</button> }
+    {page!==total && <button onClick={handlePageNext}>Next</button> }
+  </div>
     </main>
   );
 }
